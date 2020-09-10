5974204ca21005d0c950aa83b87e1afcb59044fb
Allison MacQueen <allimacq@gmail.com>
2020-09-10 23:54:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  #setter property
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  #getter property
+  def name
+    @this_dogs_name
+  end
+  
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
